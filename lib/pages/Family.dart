35d22caef73296063f50915d7d2dbc513c1fdf4e
世兴 欧 <1653756334@qq.com>
2022-03-03 11:26:59 +0800

    消息Item

--- lib/pages/Family.dart
+++ lib/pages/Family.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:guardian/utils/GetColor.dart';
 import 'package:guardian/utils/ScreenAdapter.dart';
 import 'package:guardian/widgets/MessageItem.dart';
 
@@ -14,9 +15,31 @@ class _FamilyState extends State<Family> {
   Widget build(BuildContext context) {
     ScreenAdapter.init(context);
     return Container(
-      width: double.infinity,
-      height: double.infinity,
-      child: Container(child: Text("家庭"),)
-    );
+        width: double.infinity,
+        height: double.infinity,
+        color: GetColorFrom("#ffffff"),
+        child: Container(
+          child: ListView(
+            padding: EdgeInsets.only(top: 10),
+            children: [
+              MessageItem(
+                withIcon: true,
+                name: "爸爸",
+                message: "回家了",
+                time: "12:09",
+                imgUrl:
+                    "https://img.iplaysoft.com/wp-content/uploads/2019/free-images/free_stock_photo.jpg!0x0.webp",
+              ),
+              MessageItem(
+                withIcon: false,
+                name: "妈妈",
+                message: "回家了",
+                time: "12:09",
+                imgUrl:
+                    "https://img.iplaysoft.com/wp-content/uploads/2019/free-images/free_stock_photo.jpg!0x0.webp",
+              ),
+            ],
+          ),
+        ));
   }
 }
--- lib/widgets/MessageItem.dart
+++ lib/widgets/MessageItem.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:guardian/utils/GetColor.dart';
+import 'package:guardian/utils/ScreenAdapter.dart';
 
 class MessageItem extends StatefulWidget {
-  const MessageItem({Key? key}) : super(key: key);
+  String name;
+  String message;
+  String time;
+  String imgUrl;
+  bool withIcon;
+  VoidCallback? onTap;
+  MessageItem({Key? key, required this.name, required this.message, required this.time, required this.imgUrl, required this.withIcon,this.onTap}) : super(key: key);
 
   @override
   _MessageItemState createState() => _MessageItemState();
@@ -10,9 +18,68 @@ class MessageItem extends StatefulWidget {
 class _MessageItemState extends State<MessageItem> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-
+    return InkWell(
+      onTap: widget.onTap,
+      child: Container(
+        width: double.infinity,
+        height: ScreenAdapter.width(87),
+        padding: EdgeInsets.only(left: ScreenAdapter.width(15),),
+        color: GetColorFrom("#f9f9f9"),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 7,
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: ScreenAdapter.width(36),
+                    backgroundImage: NetworkImage(
+                      widget.imgUrl,
+                    ),
+                  ),
+                  SizedBox(
+                    width: ScreenAdapter.width(15),
+                  ),
+                  // 详细信息
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.name,
+                        style: TextStyle(
+                            fontSize: ScreenAdapter.width(17),
+                            color: GetColorFrom("#3d444d")),
+                      ),
+                      Text(
+                        widget.message,
+                        style: TextStyle(
+                            fontSize: ScreenAdapter.width(14),
+                            color: GetColorFrom("#8c9198")),
+                      ),
+                      Text(
+                        widget.time,
+                        style: TextStyle(color: GetColorFrom("#bdc4ce")),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  widget.withIcon ? Image.asset("assets/images/single.png") : Text(""),
+                  Icon(Icons.arrow_forward_ios_outlined,
+                    color: GetColorFrom("#8c9198"),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
